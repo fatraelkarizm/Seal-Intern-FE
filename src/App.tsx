cafb77e1b3bb767b5d90ed294456d31b2fd3366d
fatra <alkhawarizmifatra@gmail.com>
2025-07-30 00:47:03 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+
+  return (
+    <>
+    <div className="container *:mx-auto p-4">
+      <h1 className="text-3xl font-bold underline">
+        Hello, Vite + React + Tailwind CSS!
+      </h1>
+      <p className="mt-4 text-lg">
+        This is a simple setup to get you started with Vite, React, and Tailwind CSS.
+      </p>
+
+    </div>
+
+
+    </>
+  )
+}
+
+export default App
